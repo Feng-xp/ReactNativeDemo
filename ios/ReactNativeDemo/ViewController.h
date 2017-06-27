10cbb64a94613640001b302075e5dd23b319d465
qzp <qinzhipeng@jk.cn>
2017-06-27 09:50:10 +0800

    init Project

---
+++ ios/ReactNativeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReactNativeDemo
+//
+//  Created by qzp on 2017/6/26.
+//  Copyright © 2017年 qzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios/ReactNativeDemo/ViewController.m
@@ -0,0 +1,66 @@
+//
+//  ViewController.m
+//  ReactNativeDemo
+//
+//  Created by qzp on 2017/6/26.
+//  Copyright © 2017年 qzp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import <React/RCTRootView.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake((self.view.frame.size.width - 120)/2, 120, 120, 60)];
+    [button setTitle:@"走进React" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(onButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [button setBackgroundColor:[UIColor lightGrayColor]];
+    [self.view addSubview:button];
+}
+
+- (void)onButtonTapped:(UIButton *)button
+{
+    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios"];
+    
+    NSDictionary *props = @{
+                            @"scores" : @[
+                                    @{
+                                        @"name" : @"Alex",
+                                        @"value": @"42"
+                                        },
+                                    @{
+                                        @"name" : @"Joel",
+                                        @"value": @"10"
+                                        }
+                                    ]
+                            };
+    
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL: jsCodeLocation
+                                                        moduleName: @"RNHighScores"
+                                                 initialProperties:props
+                                                     launchOptions:nil];
+    
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view = rootView;
+    [self presentViewController:vc animated:YES completion:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
